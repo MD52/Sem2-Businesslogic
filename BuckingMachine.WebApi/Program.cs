cb6a74aecc9f5249c199d75f2a31824419953c85
MD52 <daniel.meyer@stud.abbts.ch>
2026-08-06 22:57:38 +0200

    Configure MySQL database context

--- BuckingMachine.WebApi/Program.cs
+++ BuckingMachine.WebApi/Program.cs
@@ -5,15 +5,36 @@ using BuckingMachine.Application.ProcessData;
 using BuckingMachine.Infrastructure.Authentication;
 using BuckingMachine.Infrastructure.Persistence;
 using BuckingMachine.Infrastructure.Simulation;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<IMachineControlGateway, MqttMachineSimulator>();
-builder.Services.AddSingleton<IProcessDataRepository, ProcessDataRepository>();
-builder.Services.AddScoped<IAuthenticationService, JwtTokenService>();
+// ======================================================
+// DATENBANKKONFIGURATION: BEGINN
+// ======================================================
 
+
+string connectionString = builder.Configuration.GetConnectionString("BuckingMachine")
+    ?? throw new InvalidOperationException(
+        "Der Connection String 'BuckingMachine' wurde nicht konfiguriert.");
+
+builder.Services.AddDbContext<BuckingMachineDbContext>(options =>
+    options.UseMySql(
+        connectionString,
+        new MySqlServerVersion(new Version(8, 0, 0))));
+
+
+builder.Services.AddScoped<IProcessDataRepository, ProcessDataRepository>();
+
+// ======================================================
+// DATENBANKKONFIGURATION: ENDE
+// ======================================================
+
+
+builder.Services.AddScoped<IAuthenticationService, JwtTokenService>();
+builder.Services.AddSingleton<IMachineControlGateway, MqttMachineSimulator>();
 builder.Services.AddScoped<AuthenticateOperatorUseCase>();
 builder.Services.AddScoped<MachineControlUseCase>();
 builder.Services.AddScoped<ReadMachineStatusUseCase>();
